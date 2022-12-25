71febeefa4d5dfd2e2daa6b0e31290ddfae60d1c
Ujjwal <ujjwaldhariwal0@gmail.com>
2022-12-26 02:20:48 +0530

    Update script.js

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
         Indexes within the board
         [0] [1] [2]
         [3] [4] [5]
-        [6] [7] [8]
+        [6] [7] [8] z z z z
     */
 
     const winningConditions = [
